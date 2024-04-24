e6072ec2979277be5db4563f082adcf87bf59219
Haile <61838024+Haile1221@users.noreply.github.com>
2024-04-24 11:20:37 +0300

    netflix first commit

---
+++ netflix-clone/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Home from './Pages/Home/HomePage'
+
+function App() {
+  return (
+    <div className='App'>
+    <h1>Hello</h1>
+       <Home/>
+    </div>
+  )
+}
+
+export default App
---
+++ netflix-clone/src/Pages/Home/HomePage.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from '../../netflixComponents/Header/Header'
+import Footer from '../../netflixComponents/Footer/Footer'
+
+const Home = () => {
+  return (
+    <> 
+    <Header/>
+    < Footer />
+
+    </>
+  )
+}
+
+export default Home
